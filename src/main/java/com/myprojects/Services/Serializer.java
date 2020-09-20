36056d3d45b3a33140a3fdb9d473997e1dfb98d6
Den <dencult@gmail.com>
2020-09-20 12:22:59 +0400

    delete POJO package

--- src/main/java/com/myprojects/Services/Serializer.java
+++ src/main/java/com/myprojects/Services/Serializer.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myprojects.FrameDrawer;
-import com.myprojects.WeatherAPP_POJOs.RequestPOJO;
+
 
 import java.io.IOException;
 
